691a0e86c4c11a9c432063aac9f926b04c81d982
PhamTrungTin123 <124122086+PhamTrungTin123@users.noreply.github.com>
2023-10-20 09:15:28 +0700

    Up lần 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // validation form login
-const inputUsername = document.querySelector(".input-login-username");
+const inputEmail = document.querySelector(".input-login-email");
 const inputPassword = document.querySelector(".input-login-password");
 const btnLogin = document.querySelector(".login__signInButton");
 
@@ -7,12 +7,12 @@ const btnLogin = document.querySelector(".login__signInButton");
 
 btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
-  if (inputUsername.value === "" || inputPassword.value === "") {
+  if (inputEmail.value === "" || inputPassword.value === "") {
     alert("vui lòng không để trống");
   } else {
-    const user = JSON.parse(localStorage.getItem(inputUsername.value));
+    const user = JSON.parse(localStorage.getItem(inputEmail.value));
     if (
-      user.username === inputUsername.value &&
+      user.email === inputEmail.value &&
       user.password === inputPassword.value
     ) {
       alert("Đăng Nhập Thành Công");
--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
 // validation form register and register user local storage
-const inputUsernameRegister = document.querySelector(".input-signup-username");
+const inputEmailRegister = document.querySelector(".input-signup-email");
 const inputPasswordRegister = document.querySelector(".input-signup-password");
 const btnRegister = document.querySelector(".signup__signInButton");
 
@@ -8,19 +8,25 @@ const btnRegister = document.querySelector(".signup__signInButton");
 btnRegister.addEventListener("click", (e) => {
   e.preventDefault();
   if (
-    inputUsernameRegister.value === "" ||
+    inputEmailRegister.value === "" ||
     inputPasswordRegister.value === ""
   ) {
     alert("vui lòng không để trống");
   } else {
     // array user
     const user = {
-      username: inputUsernameRegister.value,
+      email: inputEmailRegister.value,
       password: inputPasswordRegister.value,
     };
-    let json = JSON.stringify(user);
-    localStorage.setItem(inputUsernameRegister.value, json);
-    alert("Đăng Ký Thành Công");
-    window.location.href = "login.html";
+    const registeredUser = localStorage.getItem(inputEmailRegister.value);
+    if (registeredUser) {
+      alert("Email này đã có người đăng ký");
+    }else{
+
+      let json = JSON.stringify(user);
+      localStorage.setItem(inputEmailRegister.value, json);
+      alert("Đăng Ký Thành Công");
+      window.location.href = "login.html";
+    }
   }
 });
